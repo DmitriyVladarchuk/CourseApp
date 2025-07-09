4f046329b14e9ab3b932c253ff87c321e1732192
unknown <dimadf678@gmail.com>
2025-07-09 16:22:10 +0300

    feat: implement core domain layer

---
+++ domain/src/main/java/com/example/courseapp/domain/repository/CoursesRepository.kt
@@ -0,0 +1,10 @@
+package com.example.courseapp.domain.repository
+
+import com.example.courseapp.domain.model.Course
+import kotlinx.coroutines.flow.Flow
+
+interface CoursesRepository {
+    suspend fun getCourses(): Flow<List<Course>>
+    suspend fun addFavorite(course: Course)
+    suspend fun removeFavorite(course: Course)
+}
---
+++ domain/src/main/java/com/example/courseapp/domain/usecase/ValidateEmailUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.courseapp.domain.usecase
+
+class ValidateEmailUseCase {
+    private companion object {
+        const val EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+    }
+
+    operator fun invoke(email: String): Boolean {
+        val regex = EMAIL_REGEX.toRegex()
+        return regex.matches(email)
+    }
+}
