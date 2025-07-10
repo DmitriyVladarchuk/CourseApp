23485bf26a3fcad7556e132900190e96311d1d34
unknown <dimadf678@gmail.com>
2025-07-10 18:50:59 +0300

    add login layout

---
+++ app/src/main/java/com/example/courseapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.example.courseapp.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.courseapp.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
