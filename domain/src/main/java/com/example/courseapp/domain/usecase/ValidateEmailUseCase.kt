ef68a3f41f62212ce55ccff8921c80dcd558a553
unknown <dimadf678@gmail.com>
2025-07-11 20:38:53 +0300

    fix emailVerificationUseCase

--- domain/src/main/java/com/example/courseapp/domain/usecase/ValidateEmailUseCase.kt
+++ domain/src/main/java/com/example/courseapp/domain/usecase/ValidateEmailUseCase.kt
@@ -2,7 +2,7 @@ package com.example.courseapp.domain.usecase
 
 class ValidateEmailUseCase {
     private companion object {
-        const val EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+        const val EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$"
     }
 
     operator fun invoke(email: String): Boolean {
